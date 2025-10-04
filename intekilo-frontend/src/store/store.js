9dd7bf26ce34ba62337f0282b1c4c70c4c9d81d6
edend333 <edend333@gmail.com>
2025-10-04 21:18:20 +0300

    fix: import redux-thunk as default

--- intekilo-frontend/src/store/store.js
+++ intekilo-frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import { thunk } from 'redux-thunk'
+import thunk from 'redux-thunk'
 
 import { userReducer } from './user.reducer'
 import { reviewReducer } from './review.reducer'
